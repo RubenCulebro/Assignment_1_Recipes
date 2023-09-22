0f9be37049fba866c4529233bbac89581612e259
RubenCulebro <122282193+RubenCulebro@users.noreply.github.com>
2023-09-22 04:07:57 -0230

    Load recipe data from json file and set initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/recipes.json")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error("Error fetching recipes:", error));
+  }, []);
+
   return (
     <Router>
       <div>
