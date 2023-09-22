b1b0ffd42c8f89477a53752d3c3b46ba8c53cf34
RubenCulebro <122282193+RubenCulebro@users.noreply.github.com>
2023-09-22 04:45:06 -0230

    Local state setup for each of the form fields: name, ingredients, directions, description, and image.

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddRecipe() {
+function AddRecipe({ onAdd }) {
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [directions, setDirections] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    
   return (
     <div>
       {/* Form for adding a new recipe will be rendered here */}
