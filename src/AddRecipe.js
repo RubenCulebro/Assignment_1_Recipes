d8c8eefe1ddc9053482e7b7984d60b6e14b177da
RubenCulebro <122282193+RubenCulebro@users.noreply.github.com>
2023-09-22 05:28:26 -0230

    Create form user interface and set up form submission handling

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function AddRecipe({ onAdd }) {
     const [name, setName] = useState("");
@@ -7,11 +8,57 @@ function AddRecipe({ onAdd }) {
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
     
-  return (
-    <div>
-      {/* Form for adding a new recipe will be rendered here */}
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+  
+        const newRecipe = {
+            name,
+            ingredients,
+            directions,
+            description,
+            image
+        };
+    
+        onAdd(newRecipe);
+        navigate('/');
+    };
+  
+    return (
+        <div>
+            <h2>Add new recipe</h2>
+            <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div>
+                <label>Ingredients:</label>
+                <textarea value={ingredients} onChange={(e) => setIngredients(e.target.value)} required />
+            </div>
+            <div>
+                <label>Directions:</label>
+                <textarea value={directions} onChange={(e) => setDirections(e.target.value)} required />
+            </div>
+            <div>
+                <label>Description:</label>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
+            </div>
+            <div>
+                <label>Image:</label>
+                <select value={image} onChange={(e) => setImage(e.target.value)} required>
+                <option value="">Select an image</option>
+                <option value="images/banana_pancakes.jpg">Banana Pancakes</option>
+                <option value="images/grilled_cheese.jpg">Grilled Cheese</option>
+                </select>
+            </div>
+            <div>
+                <button type="submit">Add Recipe</button>
+            </div>
+            </form>
+        </div>
+    );
 }
 
 export default AddRecipe;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './RecipeList';
+import AddRecipe from './AddRecipe';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -11,6 +12,10 @@ function App() {
       .then((data) => setRecipes(data))
       .catch((error) => console.error("Error fetching recipes:", error));
   }, []);
+
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
 
   const removeRecipe = (recipeName) => {
     setRecipes(recipes.filter(recipe => recipe.name !== recipeName));
@@ -27,7 +32,7 @@ function App() {
         </nav>
         <Routes>
           <Route path="/" element={<RecipeList recipes={recipes} onRemove={removeRecipe} />} />
-          {/* AddRecipe route */}
+          <Route path="/add" element={<AddRecipe onAdd={addRecipe} />} />
         </Routes>
       </div>
     </Router>
