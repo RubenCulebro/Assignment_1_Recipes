37d910510a1d84dacebb0c39f933cb7f2d7161f4
RubenCulebro <122282193+RubenCulebro@users.noreply.github.com>
2023-09-22 02:51:34 -0230

    Create basic structure for RecipeList and AddRecipe components

---
+++ src/AddRecipe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddRecipe() {
+  return (
+    <div>
+      {/* Form for adding a new recipe will be rendered here */}
+    </div>
+  );
+}
+
+export default AddRecipe;
---
+++ src/RecipeList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecipeList() {
+  return (
+    <div>
+      {/* Recipe list will be rendered here */}
+    </div>
+  );
+}
+
+export default RecipeList;
